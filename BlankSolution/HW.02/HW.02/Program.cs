75ff8f762b1736776c6dba18ad0913d19dfef5bd
KrystsinaKalkouskaya <moszechok@gmail.com>
2019-12-02 18:21:56 +0300

    Add text algorithm to solution

---
+++ BlankSolution/HW.02/HW.02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Read file by path D:\Projects\ITACADEMY\image.txt.
+            //2. Read all characters from the current position to the end of the stream.
+            //3. Create an array from string splitted by empty space.
+            //4. Create an array which size is a total number of elements in arrayOfTextResult - 1.
+            //5. Clear memory from the textReader object.
+            //6. Initialize for loop which starts from 0, ends by arrayOfTextResult.Length - 1. Loop step = 1. Then do following steps on each iteration:
+                //6.1. Convert each item of arrayOfTextResult to an 8-bit integer, write this value to "binary" variable.
+                //6.2. Assign to i-th element of imageBytes array "binary" value.
+            //7. Create a new file image.png (file path: D:\Photo\image.png), write the specified byte array to the file, and then close the file
+        }
+    }
+}
