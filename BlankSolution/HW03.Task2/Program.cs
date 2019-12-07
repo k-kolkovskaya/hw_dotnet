4e34dbd55dc23d741b14ab4924fd96ff4fc6892a
Businka2611 <moszechok@gmail.com>
2019-12-08 00:14:35 +0300

    Add HW03.Task2

---
+++ BlankSolution/HW03.Task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HW03.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte @sbyte = 12;
+            SByte @SByte = 13;
+
+            short @short = 12;
+            Int16 @Int16 = 12;
+
+            int @int = 12;
+            Int32 @Int32 = 12;
+
+            long @long = 123;
+            Int64 @Int64 = 123;
+
+            byte @byte = 121;
+            Byte @Byte = 123;
+
+            ushort @ushort = 12;
+            UInt16 @UInt16 = 12;
+
+            char @char = '1';
+            Char @Char = '1';
+
+            uint @uint = 4565454;
+            UInt32 @UInt32 = 4546454;
+
+            ulong @ulong = 455654547;
+            UInt64 @UInt64 = 5454678979;
+
+            float @float = 45;
+            Single @Single = 456;
+
+            double @double = 4.5;
+            Double @Double = 4.56;
+
+            decimal @decimal = 45454;
+            Decimal @Decimal = 456546;
+
+            string @string = "Hello";
+            String @String = "Hello";
+
+            object @object = 1;
+            Object @Object = 1;
+
+        }
+    }
+}
