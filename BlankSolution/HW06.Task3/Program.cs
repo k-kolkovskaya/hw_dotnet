f6c025e5846da40491057c8889031d1f7acf3471
Businka2611 <moszechok@gmail.com>
2019-12-22 22:25:04 +0300

    Add Task 3

---
+++ BlankSolution/HW06.Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HW06.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < (arr.Length - 1)/2; i++)
+            {
+                int firstValue = arr[i];
+                arr[i] = arr[arr.Length - 1 - i];
+                arr[arr.Length - 1 - i] = firstValue;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
