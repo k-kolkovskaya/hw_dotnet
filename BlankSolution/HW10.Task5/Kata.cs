eb0d2c236e89a373b5961757bd19dce6e7a7970d
KrystsinaKalkouskaya <moszechok@gmail.com>
2020-01-20 13:26:15 +0300

    Add Task 5

---
+++ BlankSolution/HW10.Task5/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HW10.Task5
+{
+    class Kata
+    {
+        public int[] integerList;
+        public int[] valuesList;
+        public Kata(int[] integerList, int[] valuesList)
+        {
+            this.integerList = integerList;
+            this.valuesList = valuesList;
+        }
+        public static int[] Remove(int[] integerList, int[] valuesList)
+        {
+            return integerList.Where(x => !valuesList.Contains(x)).ToArray();
+        }
+    }
+}
