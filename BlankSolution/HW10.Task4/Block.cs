5150bc8dd0e4a09f5703f0ba14457dc86987add2
KrystsinaKalkouskaya <moszechok@gmail.com>
2020-01-20 12:24:43 +0300

    Add Task 4

---
+++ BlankSolution/HW10.Task4/Block.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW10.Task4
+{
+    class Block
+    {
+        public int width;
+        public int length;
+        public int height;
+        public Block(int[] args)
+        {
+            width = args[0];
+            length = args[1];
+            height = args[2];
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public int GetHeight()
+        {
+            return height;
+        }
+
+        public int GetVolume()
+        {
+            return (width * length * height);
+        }
+
+        public int GetSurfaceArea()
+        {
+            return 2 * (width * length + length * height + width * height);
+        }
+    }
+}
---
+++ BlankSolution/HW10.Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW10.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Block block = new Block(new int[] { 5, 8, 13 });
+
+            block.GetLength();
+        }
+    }
+}
