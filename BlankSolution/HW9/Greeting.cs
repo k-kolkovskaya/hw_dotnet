701d68ee5c15847647d1b5e525f454b26a88bf37
KrystsinaKalkouskaya <moszechok@gmail.com>
2020-01-20 14:12:35 +0300

    Initial commit

---
+++ BlankSolution/HW9/Greeting.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW9
+{
+    class Greeting
+    {
+        
+    }
+}
---
+++ BlankSolution/HW9/Passenger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW9
+{
+    class Passenger
+    {
+        public string FullName { get; set; }
+        public int DateOfBirth { get; set; }
+        public string Passport { get; set; }
+        public string Flight { get; set; }
+
+        public Passenger(string fullName, int dateOfBirth, string passport, string flight)
+        {
+            FullName = fullName;
+            DateOfBirth = dateOfBirth;
+            Passport = passport;
+            Flight = flight;
+        }
+
+        
+    }
+}
