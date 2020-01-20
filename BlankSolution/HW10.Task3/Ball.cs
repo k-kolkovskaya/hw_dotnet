d13108224283403080928cc713919682a4239b61
KrystsinaKalkouskaya <moszechok@gmail.com>
2020-01-20 12:05:34 +0300

    Add Task 3

---
+++ BlankSolution/HW10.Task3/Ball.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW10.Task3
+{
+    public class Ball
+    {
+        public string ballType { get; set; }
+
+        public Ball(string ballType = "regular")
+        {
+            this.ballType = ballType;
+        }
+    }
+}
---
+++ BlankSolution/HW10.Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HW10.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ball ball1 = new Ball();
+            Console.WriteLine(ball1.ballType);
+
+            //Regular Ball Super Ball
+            //Create a class Ball.
+
+            //Ball objects should accept one argument for "ball type" when instantiated.
+
+            //If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+            //ball1 = new Ball();
+            //ball2 = new Ball("super");
+
+            //ball1.ballType     //=> "regular"
+            //ball2.ballType     //=> "super"
+
+        }
+}
+}
