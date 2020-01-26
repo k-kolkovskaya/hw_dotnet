43152fd4c99a1bcdc95982f90322f9178e3ada08
Businka2611 <moszechok@gmail.com>
2020-01-26 15:32:18 +0300

    Add sollution

---
+++ BlankSolution/HW11/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW11
+{
+    class Circle : Base
+    {
+        public Circle(int side) : base(side, "circle")
+        {
+
+        }
+
+        public override double GetSquare()
+        {
+            return Math.Round(((Math.PI) * Math.Pow(Side, 2)), 3);
+        }
+    }
+}
---
+++ BlankSolution/HW11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle round1 = new Circle(10);
+            Circle round2 = new Circle(2);
+            Triangle triangle = new Triangle(1);
+
+            Base[] figureObject = { round1, round2, triangle };
+
+            foreach (Base item in figureObject)
+            {
+                Console.WriteLine($"This is {item.Name}. CLR Type is {item.ToString()}. Square is {item.GetSquare()}");
+            }
+        }
+    }
+}
