135471910a6ea51e3e812f24d6f9a363ab9888ae
KrystsinaKalkouskaya <moszechok@gmail.com>
2020-02-03 13:28:12 +0300

    Add log to sollution

--- BlankSolution/HW11/Program.cs
+++ BlankSolution/HW11/Program.cs
@@ -7,17 +7,23 @@ namespace HW11
         static void Main(string[] args)
         {
             Logger.InitLogger();
-            Logger.Log.Info("This is my info message");
+            Logger.Log.Info($"{typeof(Program).Namespace} starts");
             Circle round1 = new Circle(10);
-            Circle round2 = new Circle(2);
+            Logger.Log.Info("New instance of Circle was created");
+            Square square = new Square(2);
+            Logger.Log.Info("New instance of Square was created");
             Triangle triangle = new Triangle(1);
+            Logger.Log.Info("New instance of Triangle was created");
 
-            Base[] figureObject = { round1, round2, triangle };
+            Base[] figureObject = { round1, square, triangle };
 
+            Logger.Log.Info("Output to console starts");
             foreach (Base item in figureObject)
             {
                 Console.WriteLine($"This is {item.Name}. CLR Type is {item.ToString()}. Square is {item.GetSquare()}");
             }
+
+            Logger.Log.Info($"{typeof(Program).Namespace} finishes");
         }
     }
 }
